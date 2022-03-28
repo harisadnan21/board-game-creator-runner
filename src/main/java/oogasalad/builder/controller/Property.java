3d2bd69680ebea50ab4f51f0bcb932f553d55230
Ricky Weerts <rrw24@duke.edu>
2022-03-27 21:52:18 -0400

    Implement Property class

--- src/main/java/oogasalad/builder/controller/Property.java
+++ src/main/java/oogasalad/builder/controller/Property.java
@@ -5,27 +5,53 @@ import java.util.*;
 /**
  * 
  */
-public class Property {
+public class Property<T> {
+    private Class<T> type;
 
-    /**
-     * Default constructor
-     */
-    public Property() {
+    private String name;
+
+    private T value;
+
+    public Property(Class<T> type, String name) {
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(name);
+        this.type = type;
     }
 
-    /**
-     * 
-     */
-    private void String type;
+    public Class<T> getType() {
+        return type;
+    }
 
-    /**
-     * 
-     */
-    private void String name;
+    public String getName() {
+        return name;
+    }
 
-    /**
-     * 
-     */
-    private void T value;
+    public T getValue() {
+        return value;
+    }
 
+    public void setValue(T newValue) {
+        Objects.requireNonNull(newValue);
+        if(!type.equals(newValue.getClass())) {
+            throw new IllegalArgumentException(ExceptionResourcesSingleton.getInstance().getString("BadPropertyValueType"));
+        }
+        value = newValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Property<?> property = (Property<?>) o;
+        return type.equals(property.type) && name.equals(property.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, name);
+    }
 }
