d4bcae4d76e7066b0819c0d2bea4e91d367bd318
Mike Keohane <mbk28@duke.edu>
2022-03-27 22:58:13 -0400

    Made a builder Main and an example for how tabs would work in builder view

--- src/main/java/oogasalad/builder/view/BuilderView.java
+++ src/main/java/oogasalad/builder/view/BuilderView.java
@@ -1,6 +1,14 @@
 package oogasalad.builder.view;
 
 import java.util.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 /**
  * 
@@ -10,18 +18,41 @@ public class BuilderView {
     /**
      * Default constructor
      */
-    public BuilderView() {
+    public BuilderView(Stage stage) {
+        exampleTabStuff(stage);
     }
 
 
 
-    /**
-     * @param element 
-     * @return
-     */
-    public void putGameElement(ElementRecord element) {
-        // TODO implement here
-        return null;
+//    /**
+//     * @param element
+//     * @return
+//     */
+//    public void putGameElement(ElementRecord element) {
+//        // TODO implement here
+//        return null;
+//    }
+
+    //Temp method just to show how to create and manage tabs
+    private void exampleTabStuff(Stage stage){
+        TabPane tabPane = new TabPane();
+        Tab tab = new Tab();
+        tab.setText("tab");
+        BorderPane tabObj = new BorderPane();
+        tabObj.setCenter(new Rectangle(200,200, Color.BLUE));
+        tab.setContent(tabObj);
+        tabPane.getTabs().add(tab);
+
+        Tab tab2 = new Tab();
+        tab2.setText("tab2");
+        tab2.setContent(new Rectangle(200,200, Color.GREEN));
+        tabPane.getTabs().add(tab2);
+
+        tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+
+        Scene myScene = new Scene(tabPane, 200, 200);
+        stage.setScene(myScene);
+        stage.show();
     }
 
 }
