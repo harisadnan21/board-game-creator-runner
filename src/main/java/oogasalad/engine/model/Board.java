572670d3f793e3e097a27d2a94af3ff6d34381a6
Robert Cranston <rvc6@duke.edu>
2022-03-27 22:05:36 -0400

    added duval's obeservable class

--- src/main/java/oogasalad/engine/model/Board.java
+++ src/main/java/oogasalad/engine/model/Board.java
@@ -1,5 +1,24 @@
 package oogasalad.engine.model;
 
-public class Board {
+import java.util.List;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+
+public class Board extends Observable<List>{
+
+  private int myHeight;
+  private int myWidth;
+  private int[][] myBoard;
+
+  public Board(int width, int height) {
+    myHeight = height;
+    myWidth = width;
+    myBoard = new int[height][width];
+  }
+
+  public Board selectCell(int x, int y){
+
+  }
+
 
 }
