0ce8f8e6267ffe2a5a94ed9cc5e13806dda5b831
Robert Cranston <rvc6@duke.edu>
2022-03-27 22:23:30 -0400

    adding single piece to board

--- src/main/java/oogasalad/engine/model/Board.java
+++ src/main/java/oogasalad/engine/model/Board.java
@@ -4,17 +4,28 @@ import java.util.List;
 import javafx.beans.InvalidationListener;
 
 
-public class Board extends Observable<Integer[][]>{
+public class Board extends Observable<Piece[][]>{
 
   private int myHeight;
   private int myWidth;
-  private int[][] myBoard;
+  private Piece[][] myBoard;
 
   public Board(int width, int height) {
     myHeight = height;
     myWidth = width;
-    myBoard = new int[height][width];
+    myBoard = new Piece[height][width];
   }
+
+  public void selectCell(int x, int y){
+    Piece[][]oldBoard = myBoard;
+    place(x, y);
+    notifyListeners("UPDATE", oldBoard, myBoard);
+  }
+
+  private void place(int x, int y){
+    myBoard[x][y] = new Piece("Knight", 1);
+  }
+
 
 
 
