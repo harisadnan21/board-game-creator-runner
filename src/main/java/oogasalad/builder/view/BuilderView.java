9af3aa503210039d7242a1feb1339f8a32ffa807
Mike Keohane <mbk28@duke.edu>
2022-03-30 14:42:37 -0500

    pulled Thivya's changes and then deleded a couple old classes + tiny maitnece

--- src/main/java/oogasalad/builder/view/BuilderView.java
+++ src/main/java/oogasalad/builder/view/BuilderView.java
@@ -14,28 +14,29 @@ import javafx.stage.Stage;
 import oogasalad.builder.view.tab.boardTab.BoardTab;
 
 /**
- * 
+ *
  */
 public class BuilderView {
-    public static double SCENE_WIDTH = 600;
-    public static double SCENE_HEIGHT = 650;
 
-    private Stage stage;
+  public static double SCENE_WIDTH = 600;
+  public static double SCENE_HEIGHT = 650;
 
-    public BuilderView(Stage mainStage) {
-        stage = mainStage;
-        setupTabs();
-        stage.show();
-    }
+  private Stage stage;
+
+  public BuilderView(Stage mainStage) {
+    stage = mainStage;
+    setupTabs();
+    stage.show();
+  }
 
 
-    /**
-     * @param element
-     * @return
-     */
-    public void putGameElement(ElementRecord element) {
-        // TODO implement here
-        //return null;
+  /**
+   * @param element
+   * @return
+   */
+  public void putGameElement(ElementRecord element) {
+    // TODO implement here
+    //return null;
 //    /**
 //     * @param element
 //     * @return
@@ -44,26 +45,26 @@ public class BuilderView {
 //        // TODO implement here
 //        return null;
 //    }
-    }
+  }
 
-        private void setupTabs() {
-            TabPane tabPane = new TabPane();
+  // Can possibly extend tab for each of the tab classes instead of just having toNode() to add them to the tabs
+  private void setupTabs() {
+    TabPane tabPane = new TabPane();
 
-            BoardTab boardTabPane = new BoardTab();
-            Tab boardTab = new Tab("Board", boardTabPane.toNode());
+    BoardTab boardTabPane = new BoardTab();
+    Tab boardTab = new Tab("Board", boardTabPane.toNode());
 
-            Tab tab2 = new Tab();
-            tab2.setText("tab2");
-            tab2.setContent(new Rectangle(600, 600, Color.GREEN));
+    Tab tab2 = new Tab();
+    tab2.setText("tab2");
+    tab2.setContent(new Rectangle(600, 600, Color.GREEN));
 
+    tabPane.getTabs().addAll(boardTab, tab2);
 
-            tabPane.getTabs().addAll(boardTab, tab2);
+    tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
 
-            tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+    //TODO get this size from file and add more to the scene
+    Scene myTabScene = new Scene(tabPane, 600, 650);
+    stage.setScene(myTabScene);
+  }
 
-            //TODO get this size from file and add more to the scene
-            Scene myTabScene = new Scene(tabPane, 600, 650);
-            stage.setScene(myTabScene);
-        }
-
-    }
+}
