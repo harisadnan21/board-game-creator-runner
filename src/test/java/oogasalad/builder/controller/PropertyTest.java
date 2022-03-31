6bf74f6f9e2489177398179681408bb3d617dbec
Ricky Weerts <rrw24@duke.edu>
2022-03-31 16:07:55 -0400

    Finished Property tests

--- src/test/java/oogasalad/builder/controller/PropertyTest.java
+++ src/test/java/oogasalad/builder/controller/PropertyTest.java
@@ -11,7 +11,31 @@ public class PropertyTest {
             prop4 = new Property(Integer.class, "different field name", "123");
 
     @Test
-    void equals() {
+    void testTypeChecking() {
+        assertThrows(IllegalArgumentException.class, () -> prop1.withValue(123));
+        assertThrows(IllegalArgumentException.class, () -> prop4.withValue("123"));
+        assertThrows(IllegalArgumentException.class, () -> prop4.withValue("hello"));
+    }
+
+    @Test
+    void testNewInstance() {
+        assertFullEquals(new Property(String.class, "test", "testVal"), Property.newInstance(String.class, "test", "testVal"));
+        assertFullEquals(new Property(Integer.class, "test", "123"), Property.newInstance(Integer.class, "test", 123));
+    }
+
+    @Test
+    void testWithValue() {
+        assertEquals("good value", prop1.withValue("good value").value());
+        assertEquals(prop1, prop1.withValue("good value"));
+        assertEquals("999", prop4.withValue(999).value());
+        assertEquals(prop4, prop4.withValue(999));
+        assertEquals("999", prop4.withValueAsString("999").value());
+        assertEquals(prop4, prop4.withValueAsString("999"));
+        assertFullEquals(prop2, prop1.withValue("other value"));
+    }
+
+    @Test
+    void testEquals() {
         assertEquals(prop1, prop1);
         assertEquals(prop1, prop2);
         assertNotEquals(prop1, prop3);
@@ -22,7 +46,18 @@ public class PropertyTest {
     }
 
     @Test
-    void fullEquals() {
+    void testHashCode() {
+        assertEquals(prop1.hashCode(), prop1.hashCode());
+        assertEquals(prop1.hashCode(), prop2.hashCode());
+        assertNotEquals(prop1.hashCode(), prop3.hashCode());
+        assertNotEquals(prop2.hashCode(), prop3.hashCode());
+        assertNotEquals(prop1.hashCode(), prop4.hashCode());
+        assertNotEquals(prop2.hashCode(), prop4.hashCode());
+        assertNotEquals(prop3.hashCode(), prop4.hashCode());
+    }
+
+    @Test
+    void testFullEquals() {
         assertFullEquals(prop1, prop1);
         assertNotFullEquals(prop1, prop2);
         assertNotFullEquals(prop1, prop3);
