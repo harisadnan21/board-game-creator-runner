af85b865c1faf9d3bd8e3787fefd8e355c70f580
Cynthia France <cf219@duke.edu>
2022-03-31 07:59:50 -0400

    added missing import

--- src/main/java/oogasalad/engine/model/futureclasses/PositionsTrait.java
+++ src/main/java/oogasalad/engine/model/futureclasses/PositionsTrait.java
@@ -1,5 +1,7 @@
 package oogasalad.engine.model.futureclasses;
 
+import oogasalad.engine.model.board.Position;
+
 @FunctionalInterface
 public interface PositionsTrait {
     public Boolean positionsTrait(Position[] positions);
