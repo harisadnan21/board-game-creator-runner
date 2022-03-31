6ab9bfb078e058250b92ad8dbc839ccb693e6b13
Mike Keohane <mbk28@duke.edu>
2022-03-31 15:52:52 -0500

    enabled clicking to add pieces (rn just ovals) to the board and also updates an int[][] storing whether there is a piece there

--- src/main/java/oogasalad/builder/view/tab/boardTab/BoardTab.java
+++ src/main/java/oogasalad/builder/view/tab/boardTab/BoardTab.java
@@ -2,6 +2,7 @@ package oogasalad.builder.view.tab.boardTab;
 
 import java.util.*;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
 /**
@@ -10,12 +11,18 @@ import javafx.scene.layout.BorderPane;
 public class BoardTab {
 
     private BorderPane boardPane;
-
+    private BoardCanvas boardCanvas;
     public BoardTab() {
         boardPane = new BorderPane();
 
         // TODO : Make this not magic
         setupBoard(8, 8, "Checkers");
+
+        //TODO : Make a section for buttons and do this right
+        Button saveButton = new Button("Print Config");
+        saveButton.setOnAction(e -> saveBoardConfig());
+        boardPane.setRight(saveButton);
+
     }
 
     public Node toNode(){
@@ -23,7 +30,11 @@ public class BoardTab {
     }
 
     private void setupBoard(int xSize, int ySize, String boardType){
-        BoardCanvas boardCanvas = new BoardCanvas(xSize, ySize);
-        boardPane.setCenter(boardCanvas.toNode());
+        boardCanvas = new BoardCanvas(xSize, ySize);
+        boardPane.setCenter(boardCanvas.getCanvasNode());
+    }
+
+    private void saveBoardConfig(){
+        System.out.println(boardCanvas.printBoardConfig());
     }
 }
