928c91455f3790dd1fa73b29e5018e67015894a6
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-04-01 00:34:48 -0400

    Added serializable interface

---
+++ src/main/java/oogasalad/builder/model/parser/JSONSerializable.java
@@ -0,0 +1,26 @@
+package oogasalad.builder.model.parser;
+
+
+/**
+ * API For adapting Back-End Objects to their JSON Format
+ *
+ * @author Shaan Gondalia
+ */
+public interface JSONSerializable<T> {
+
+  /**
+   * Converts the object into a String representing the object's JSON Format
+   *
+   * @return a String representation of the objects JSON Format
+   */
+  String toJSON();
+
+  /**
+   * Converts a JSON String into an object
+   *
+   * @param json the JSON string
+   * @return an object of type T made from the JSON string
+   */
+  T fromJSON(String json);
+
+}
