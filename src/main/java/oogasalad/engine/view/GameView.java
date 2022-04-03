f731fe33f0cc6712fa2a857d936c70f6601791e9
Cynthia France <cf219@duke.edu>
2022-04-03 10:41:11 -0400

    change view layout

--- src/main/java/oogasalad/engine/view/GameView.java
+++ src/main/java/oogasalad/engine/view/GameView.java
@@ -2,7 +2,9 @@ package oogasalad.engine.view;
 
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import oogasalad.engine.controller.Controller;
 
@@ -13,32 +15,28 @@ public class GameView {
   BoardView myBoard;
   Controller myController;
 
-  GridPane root;
-  Text updateText;
-  Inventory pInventory;
+  //GridPane root;
+  BorderPane myRoot;
 
   public GameView(BoardView board, Controller controller) {
     myBoard = board;
     myController = controller;
-    updateText = myBoard.getText();
-    pInventory = new Inventory();
-    setUpGrid();
+    myRoot = new BorderPane();
+    //setUpGrid();
     board.addController(myController);
   }
 
   public Scene makeScene() {
-    root.add(myBoard.getRoot(), 0, 0, 4, 4);
-    root.add(updateText, 5, 2);
-    root.add(pInventory.getRoot(), 5, 3, 6, 4);
-    Scene scene = new Scene(root, WIDTH, HEIGHT);
+    myRoot.setCenter(myBoard.getRoot());
+    Scene scene = new Scene(myRoot, WIDTH, HEIGHT);
+    scene.setFill(Color.web("#EEEEEE"));
     return scene;
   }
 
-  private void setUpGrid() {
-    root = new GridPane();
-    root.setHgap(10);
-    root.setVgap(10);
-    root.setPadding(new Insets(10, 10, 10, 10));
-    //root.setGridLinesVisible(true);
-  }
+//  private void setUpGrid() {
+//    root = new GridPane();
+//    root.setHgap(10);
+//    root.setVgap(10);
+//    root.setPadding(new Insets(10, 10, 10, 10));
+//  }
 }
