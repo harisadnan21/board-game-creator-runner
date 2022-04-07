d34cc6fc6b6f8a53db5d3a249f05db492adcf4ba
Mike Keohane <mbk28@duke.edu>
2022-04-06 20:40:59 -0400

    changed functionality for clicking to choose different piece types and eraser options

--- src/main/java/oogasalad/builder/view/tab/boardTab/BoardCanvas.java
+++ src/main/java/oogasalad/builder/view/tab/boardTab/BoardCanvas.java
@@ -8,6 +8,7 @@ import java.util.function.Consumer;
 import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ComboBox;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -29,6 +30,7 @@ public class BoardCanvas {
   private double rectHeight;
   private int[][] containsPiece;
   private BorderPane borderPane;
+  private String currentPiece;
 
   public BoardCanvas(ResourceBundle rb, BorderPane boardTab) {
     resources = rb;
@@ -36,7 +38,6 @@ public class BoardCanvas {
 
     setupBoard();
     populateBoardTypeMap();
-    addClickHandeling();
   }
 
   public void setColor(Paint color, int colorNum){
@@ -59,6 +60,7 @@ public class BoardCanvas {
 
     if (boardTypeFunctionMap.containsKey(type)){
       boardTypeFunctionMap.get(type).accept(new int[]{xDim, yDim});
+      setClickToPlace();
     }
     else{
       //TODO : THROW EXCEPTION
@@ -127,28 +129,50 @@ public class BoardCanvas {
     }
   }
 
-  private void addClickHandeling(){
+
+  public void setCurrentPiece(String pieceName){
+    currentPiece = pieceName;
+  }
+
+  public void setClickToErase(){
+    pieceCanvas.setOnMouseClicked(e -> erasePiece(e));
+  }
+  public void setClickToPlace(){
     pieceCanvas.setOnMouseClicked(e -> addPiece(e));
   }
 
+
+  private void erasePiece(MouseEvent click){
+    int[] blockIndex = findSquare(click.getX(), click.getY());
+    pieceGraphics.clearRect(blockIndex[0] * rectWidth, blockIndex[1] * rectHeight, rectWidth, rectHeight);
+  }
+
   private void addPiece(MouseEvent click){
+
+    if (currentPiece == null){
+      System.out.println("No piece Selected");
+      return;
+    }
+
     double clickX = click.getX();
     double clickY = click.getY();
 
     int[] blockIndex = findSquare(clickX, clickY);
     System.out.println(" xPos: " + blockIndex[0] + " yPos: " + blockIndex[1]);
 
-    // TODO: ADD OPTIONS FOR ADDING PIECES THEN ACTUALLY ADD PIECES
-    if (containsPiece[blockIndex[0]][blockIndex[1]] > 0){
-      pieceGraphics.clearRect(blockIndex[0] * rectWidth, blockIndex[1] * rectHeight, rectWidth, rectHeight);
-      containsPiece[blockIndex[0]][blockIndex[1]] = 0;
-    }
-    else{
-      pieceGraphics.setFill(Color.BLUE);
-      pieceGraphics.fillOval(blockIndex[0] * rectWidth, blockIndex[1] * rectHeight, rectWidth, rectHeight);
-      containsPiece[blockIndex[0]][blockIndex[1]] = 1;
 
-    }
+     // pieceGraphics.drawImage(currentPiece.getImage());
+    // TODO: ADD OPTIONS FOR ADDING PIECES THEN ACTUALLY ADD PIECES
+//    if (containsPiece[blockIndex[0]][blockIndex[1]] > 0){
+//      pieceGraphics.clearRect(blockIndex[0] * rectWidth, blockIndex[1] * rectHeight, rectWidth, rectHeight);
+//      containsPiece[blockIndex[0]][blockIndex[1]] = 0;
+//
+//    }
+
+    pieceGraphics.setFill(Color.BLUE);
+    pieceGraphics.fillOval(blockIndex[0] * rectWidth, blockIndex[1] * rectHeight, rectWidth, rectHeight);
+    containsPiece[blockIndex[0]][blockIndex[1]] = 1;
+
 
   }
 
