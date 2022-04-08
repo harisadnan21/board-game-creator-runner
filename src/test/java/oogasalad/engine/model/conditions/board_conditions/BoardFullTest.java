2323aa482541555bd4377a12a7fe0fa91b6e77bd
Alex Bildner <alexander.bildner@duke.edu>
2022-04-08 01:13:58 -0400

    added tests

---
+++ src/test/java/oogasalad/engine/model/conditions/board_conditions/BoardFullTest.java
@@ -0,0 +1,36 @@
+package oogasalad.engine.model.conditions.board_conditions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Iterator;
+import java.util.PrimitiveIterator.OfInt;
+import java.util.stream.IntStream;
+import oogasalad.engine.model.board.Board;
+import oogasalad.engine.model.board.Position;
+import oogasalad.engine.model.board.PositionState;
+import org.jooq.lambda.tuple.Range;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BoardFullTest {
+
+  @Test
+  void isTrue() throws CloneNotSupportedException {
+    Board board = getBoard();
+    BoardFull boardFull = new BoardFull();
+    Assertions.assertTrue(boardFull.isTrue(board));
+  }
+
+  private Board getBoard() throws CloneNotSupportedException {
+    Board currentBoard = new Board(4, 4);
+    for (OfInt it = IntStream.rangeClosed(1, 4).iterator(); it.hasNext(); ) {
+      int x = it.next();
+      for (OfInt it1 = IntStream.rangeClosed(1, 4).iterator(); it1.hasNext(); ) {
+        int y = it1.next();
+        PositionState positionState = new PositionState(new Position(x, y), 1, "normal");
+        currentBoard = currentBoard.placePiece(positionState);
+      }
+    }
+    return currentBoard;
+  }
+}
