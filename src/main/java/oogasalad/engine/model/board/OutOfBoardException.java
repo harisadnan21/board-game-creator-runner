4ef821f2a15cd9fc44b97864abd2be0ba03707c0
Alex Bildner <alexander.bildner@duke.edu>
2022-04-08 04:47:08 -0400

    added convenience methods to ray

--- src/main/java/oogasalad/engine/model/board/OutOfBoardException.java
+++ src/main/java/oogasalad/engine/model/board/OutOfBoardException.java
@@ -1,6 +1,7 @@
 package oogasalad.engine.model.board;
 
 public class OutOfBoardException extends IndexOutOfBoundsException {
+  //TODO: make sure this message is informative
   public OutOfBoardException(String message) {
     super(message);
   }
--- src/main/java/oogasalad/engine/model/board/Ray.java
+++ src/main/java/oogasalad/engine/model/board/Ray.java
@@ -1,16 +1,27 @@
 package oogasalad.engine.model.board;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.Stream.Builder;
 import oogasalad.engine.model.setup.Constants;
 import oogasalad.engine.model.setup.Delta;
+import org.jooq.lambda.Seq;
 
 public class Ray {
 
   public static Stream<PositionState> getDirectionalRay(Board board, Position position, Direction direction) {
     return Ray.getDirectionalRayWhileCondition(board, position, direction, positionState -> true);
+  }
+
+  public static Map<Direction, Stream<PositionState>> getDirectionalRays(Board board, Position position, Collection<Direction> directions) {
+    return Seq.seq(directions)
+              .toMap(direction -> direction,
+                     direction -> Ray.getDirectionalRayWhileCondition(board, position, direction, positionState -> true));
   }
 
 
@@ -21,20 +32,25 @@ public class Ray {
 
 
   public static Stream<PositionState> getDirectionalRayWhileConditions(Board board, Position position, Direction direction, List<Predicate<PositionState>> positionStatePredicates) {
-    Builder<PositionState> ray = Stream.<PositionState>builder();
+    Builder<PositionState> ray = Stream.builder();
     int x = position.x();
     int y = position.y();
     Delta delta = Constants.DIRECTIONDELTAS.get(direction);
-    Predicate<PositionState> positionStatePredicate = positionStatePredicates.stream().reduce(positionState -> true,
-        Predicate::and);
+    Predicate<PositionState> positionStatePredicate = reducePredicates(positionStatePredicates); // Combines list of predicates into one predicate which is when they are all true
     while(isValid(board, x, y) && passesCondition(board, positionStatePredicate, x, y)){
       ray.accept(board.getPositionStateAt(x,y));
-      x = x + delta.xdelta();
-      y = y + delta.ydelta();
+      x += delta.xdelta();
+      y += delta.ydelta();
     }
 
     return ray.build();
 
+  }
+
+  private static Predicate<PositionState> reducePredicates(
+      List<Predicate<PositionState>> positionStatePredicates) {
+    return positionStatePredicates.stream().reduce(positionState -> true,
+        Predicate::and);
   }
 
   public static Stream<PositionState> getDirectionalRayUntilCondition(Board board, Position position, Direction direction, Predicate<PositionState> positionStatePredicate) {
