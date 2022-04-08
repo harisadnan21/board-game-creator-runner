b883f6e826dc5f6810575aed090c222189d38df2
Alex Bildner <alexander.bildner@duke.edu>
2022-04-08 04:28:10 -0400

    added convenience methods to positionstate

--- src/main/java/oogasalad/engine/model/board/PositionState.java
+++ src/main/java/oogasalad/engine/model/board/PositionState.java
@@ -3,6 +3,16 @@ package oogasalad.engine.model.board;
 
 public record PositionState(Position position, int player, String pieceType) {
 
+  //Just a convenience function
+  public int x(){
+    return position.x();
+  }
+
+  //Just a convenience function
+  public int y(){
+    return position.y();
+  }
+
   @Deprecated
   public Position getKey() {
     return null;
