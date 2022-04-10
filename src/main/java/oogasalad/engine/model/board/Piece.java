9c4c992504fb9597bd37235977ef7f5bb2b01d22
Alex Bildner <alexander.bildner@duke.edu>
2022-04-09 21:03:45 -0400

    eliminated equals() method that mirrors built in equals() method

--- src/main/java/oogasalad/engine/model/board/Piece.java
+++ src/main/java/oogasalad/engine/model/board/Piece.java
@@ -14,14 +14,4 @@ public record Piece (int type, int player) {
     return new Piece(BLANK_TYPE, NO_PLAYER);
   }
 
-  public boolean equals(Object o) {
-    if (o.getClass() != Piece.class) {
-      return false;
-    }
-    Piece piece = (Piece) o;
-    if (piece.player == player() && piece.type == type()) {
-      return true;
-    }
-    return false;
-  }
 }
