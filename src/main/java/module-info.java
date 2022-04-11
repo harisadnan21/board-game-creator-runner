42f8f4d3e523bf4fb94641db326857895c78a547
Alex Bildner <alexander.bildner@duke.edu>
2022-04-11 04:04:52 -0400

    added skeleton for state evaluation memoization

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,7 @@ open module oogasalad_app {
     requires org.jooq.jool;
     requires com.google.gson;
     requires org.apache.logging.log4j;
+    requires com.github.benmanes.caffeine;
 
     // allow other classes to access listed packages in your project
     exports oogasalad;
---
+++ src/main/java/oogasalad/engine/model/AI/Evaluation/Memoize.java
@@ -0,0 +1,34 @@
+package oogasalad.engine.model.AI.Evaluation;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import oogasalad.engine.model.board.Board;
+import org.jooq.lambda.tuple.Tuple2;
+
+public class Memoize {
+  private LoadingCache<Tuple2<Board, Integer>, Integer> memoizer;
+
+  public Memoize(StateEvaluator stateEvaluator) {
+    // Resource: https://github.com/ben-manes/caffeine/wiki/Population#loading
+    memoizer = Caffeine.newBuilder()
+        .maximumSize(1_000)
+        .expireAfterWrite(20, TimeUnit.MINUTES)
+        .build(key -> stateEvaluator.Evaluate(key.v1, key.v2));
+  }
+
+  public Integer get(Board board, Integer player) {
+    // Resource: https://github.com/ben-manes/caffeine/wiki/Population#loading
+    // Lookup and compute an entry if absent, or null if not computable
+    return memoizer.get(new Tuple2<>(board, player));
+  }
+
+  public Map<Tuple2<Board, Integer>, Integer> getAll(List<Tuple2<Board, Integer>> arguments) {
+    // Resource: https://github.com/ben-manes/caffeine/wiki/Population#loading
+    // Lookup and compute entries that are absent
+    return memoizer.getAll(arguments);
+  }
+
+}
