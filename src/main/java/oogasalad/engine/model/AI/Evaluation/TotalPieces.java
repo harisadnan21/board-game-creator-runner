94e1bfc1514e73f6c7cb4816d4f5f967776a1edb
Alex Bildner <alexander.bildner@duke.edu>
2022-04-11 04:10:16 -0400

    added skeleton for state evaluation functions for total piecces

--- src/main/java/oogasalad/engine/model/AI/Evaluation/TotalPieces.java
+++ src/main/java/oogasalad/engine/model/AI/Evaluation/TotalPieces.java
@@ -1,11 +1,20 @@
 package oogasalad.engine.model.AI.Evaluation;
 
+import java.util.Map;
 import oogasalad.engine.model.board.Board;
+import oogasalad.engine.model.board.Piece;
 
 public class TotalPieces implements StateEvaluator {
 
   @Override //TODO: implement this
   public int Evaluate(Board board, int player) {
-    return 0;
+    Map<Integer, Integer> piecesByPlayer = board.numPiecesByPlayer();
+    int evaluation = 0;
+    if(player == Piece.PLAYER_ONE) {
+      
+    } else {
+      
+    }
+    return evaluation;
   }
 }
