43eb1c88af6debf140968470e7c1082acdc98c25
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-04-11 17:07:07 -0400

    Added PropertyFactoryTest and testing for different types of properties

--- src/test/java/oogasalad/builder/model/property/PropertyTest.java
+++ src/test/java/oogasalad/builder/model/property/PropertyTest.java
@@ -1,6 +1,9 @@
 package oogasalad.builder.model.property;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -12,13 +15,11 @@ public class PropertyTest {
 
   private static final String PROPERTY_NAME = "propName";
   private static final String PROPERTY_VALUE = "value";
-
-  private static final String EXPECTED_JSON = "{\"propName\":\"value\"}";
-
+  private static final String PROPERTY_FORM = "form";
 
   @Test
   void testCreateProperty() {
-    Property property = new Property(PROPERTY_NAME, PROPERTY_VALUE);
+    Property property = new Property(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
     assertEquals(PROPERTY_NAME, property.name());
     assertEquals(PROPERTY_VALUE, property.value());
     assertEquals(PROPERTY_VALUE, property.toString());
@@ -26,14 +27,14 @@ public class PropertyTest {
 
   @Test
   void testEquality() {
-    Property property1 = new Property(PROPERTY_NAME, PROPERTY_VALUE);
-    Property property2 = new Property(PROPERTY_NAME, PROPERTY_VALUE);
+    Property property1 = new Property(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
+    Property property2 = new Property(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
     assertEquals(property1, property2);
   }
 
   @Test
   void testHashCode() {
-    Property property = new Property(PROPERTY_NAME, PROPERTY_VALUE);
+    Property property = new Property(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
     int code = property.hashCode();
   }
 }
