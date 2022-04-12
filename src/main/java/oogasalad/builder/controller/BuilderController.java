ff1cceb920d414afec1af9e140c075be5b7eb5bd
Ricky Weerts <rrw24@duke.edu>
2022-04-12 17:32:42 -0400

    Changed BuilderController to have package access methods to simplify testing

--- src/main/java/oogasalad/builder/controller/BuilderController.java
+++ src/main/java/oogasalad/builder/controller/BuilderController.java
@@ -75,7 +75,7 @@ public class BuilderController {
      * @throws OccupiedCellException if the cell at x, y is already occupied by a piece
      * @throws NullBoardException    if the board has not been initialized
      */
-    private Void placePiece(PlacePieceCallback callback)
+    Void placePiece(PlacePieceCallback callback)
         throws OccupiedCellException, NullBoardException, ElementNotFoundException {
         gameConfig.placeBoardPiece(callback.x(), callback.y(), callback.piece());
         return null;
@@ -98,7 +98,7 @@ public class BuilderController {
      *
      * @param callback callback object containing the x and y location to clear
      */
-    private Void clearCell(ClearCellCallback callback) throws NullBoardException {
+    Void clearCell(ClearCellCallback callback) throws NullBoardException {
         gameConfig.clearBoardCell(callback.x(), callback.y());
         return null;
     }
@@ -109,7 +109,7 @@ public class BuilderController {
      * @param callback a callback object containing the type and name of the element to get properties for
      * @return the properties of an element
      */
-    private Collection<Property> getElementProperties(GetElementPropertiesCallback callback)
+    Collection<Property> getElementProperties(GetElementPropertiesCallback callback)
         throws ElementNotFoundException {
         ElementRecord elementRecord = gameConfig.findElementInfo(callback.type(), callback.name());
         return elementRecord.properties();
@@ -121,7 +121,7 @@ public class BuilderController {
      * @param callback callback object containing the type, name, and property key
      * @return the value of the property
      */
-    private String getElementPropertyByKey(GetElementPropertyByKeyCallback callback)
+    String getElementPropertyByKey(GetElementPropertyByKeyCallback callback)
         throws ElementNotFoundException {
         ElementRecord elementRecord = gameConfig.findElementInfo(callback.type(), callback.name());
         for (Property prop : elementRecord.properties()) {
@@ -151,7 +151,7 @@ public class BuilderController {
      *
      * @param callback a callback object containing the type, name, and properties of the element to update
      */
-    private Void update(UpdateGameElementCallback callback)
+    Void update(UpdateGameElementCallback callback)
         throws InvalidTypeException, MissingRequiredPropertyException {
        gameConfig.addGameElement(callback.type(), callback.name(), callback.properties());
        return null;
@@ -163,7 +163,7 @@ public class BuilderController {
      * @param callback a callback object cont
      * @return the required properties of a game element
      */
-    private Collection<Property> getRequiredProperties(GetPropertiesCallback callback) throws InvalidTypeException {
+    Collection<Property> getRequiredProperties(GetPropertiesCallback callback) throws InvalidTypeException {
         return gameConfig.getRequiredProperties(callback.type());
     }
 
@@ -172,7 +172,7 @@ public class BuilderController {
      *
      * @param callback callback object containing the File to save the Game Configuration to.
      */
-    private Void save(SaveCallback callback) throws NullBoardException {
+    Void save(SaveCallback callback) throws NullBoardException {
         try {
             FileWriter writer = new FileWriter(callback.file());
             writer.write(gameConfig.toJSON());
--- src/test/java/oogasalad/builder/controller/ControllerTest.java
+++ src/test/java/oogasalad/builder/controller/ControllerTest.java
@@ -16,6 +16,12 @@ import oogasalad.builder.model.exception.NullBoardException;
 import oogasalad.builder.model.exception.OccupiedCellException;
 import oogasalad.builder.model.property.PropertyFactory;
 import oogasalad.builder.view.BuilderView;
+import oogasalad.builder.view.callback.ClearCellCallback;
+import oogasalad.builder.view.callback.GetElementPropertiesCallback;
+import oogasalad.builder.view.callback.MakeBoardCallback;
+import oogasalad.builder.view.callback.PlacePieceCallback;
+import oogasalad.builder.view.callback.SaveCallback;
+import oogasalad.builder.view.callback.UpdateGameElementCallback;
 import org.junit.jupiter.api.Test;
 import util.DukeApplicationTest;
 
@@ -60,89 +66,89 @@ public class ControllerTest extends DukeApplicationTest {
 
   @Test
   void testGameElementNotFound() {
-    assertThrows(ElementNotFoundException.class, () -> controller.getElementProperties(PIECE_TYPE, PIECE_NAME));
-    assertThrows(ElementNotFoundException.class, () -> controller.getElementProperties(PIECE_TYPE, "not the name"));
+    assertThrows(ElementNotFoundException.class, () -> controller.getElementProperties(new GetElementPropertiesCallback(PIECE_TYPE, PIECE_NAME)));
+    assertThrows(ElementNotFoundException.class, () -> controller.getElementProperties(new GetElementPropertiesCallback(PIECE_TYPE, "not the name")));
   }
 
   @Test
   void testGameElementFound()
       throws ElementNotFoundException, InvalidTypeException, MissingRequiredPropertyException {
     addPiece();
-    controller.update(PIECE_TYPE, PIECE_NAME, properties);
-    Collection<Property> newProperties = controller.getElementProperties(PIECE_TYPE, PIECE_NAME);
+    controller.update(new UpdateGameElementCallback(PIECE_TYPE, PIECE_NAME, properties));
+    Collection<Property> newProperties = controller.getElementProperties(new GetElementPropertiesCallback(PIECE_TYPE, PIECE_NAME));
     assertEquals(properties,newProperties);
   }
 
   @Test
   void testNull(){
-    assertThrows(NullBoardException.class, () -> controller.placePiece(HEIGHT+1, WIDTH + 1, PIECE_NAME));
-    assertThrows(NullBoardException.class, () -> controller.clearCell(HEIGHT+1, WIDTH + 1));
+    assertThrows(NullBoardException.class, () -> controller.placePiece(new PlacePieceCallback(HEIGHT+1, WIDTH + 1, PIECE_NAME)));
+    assertThrows(NullBoardException.class, () -> controller.clearCell(new ClearCellCallback(HEIGHT+1, WIDTH + 1)));
     assertThrows(NullBoardException.class, () -> controller.findPieceAt(HEIGHT+1, WIDTH + 1));
   }
 
   @Test
   void testOutOfBounds() throws MissingRequiredPropertyException, InvalidTypeException {
-    controller.makeBoard(WIDTH, HEIGHT);
+    controller.makeBoard(new MakeBoardCallback(WIDTH, HEIGHT));
     addPiece();
-    assertThrows(IndexOutOfBoundsException.class, () -> controller.placePiece(HEIGHT+1, WIDTH + 1, PIECE_NAME));
-    assertThrows(IndexOutOfBoundsException.class, () -> controller.clearCell(HEIGHT+1, WIDTH + 1));
+    assertThrows(IndexOutOfBoundsException.class, () -> controller.placePiece(new PlacePieceCallback(HEIGHT+1, WIDTH + 1, PIECE_NAME)));
+    assertThrows(IndexOutOfBoundsException.class, () -> controller.clearCell(new ClearCellCallback(HEIGHT+1, WIDTH + 1)));
     assertThrows(IndexOutOfBoundsException.class, () -> controller.findPieceAt(HEIGHT+1, WIDTH + 1));
-    assertThrows(IndexOutOfBoundsException.class, () -> controller.placePiece(-1, WIDTH + 1, PIECE_NAME));
-    assertThrows(IndexOutOfBoundsException.class, () -> controller.clearCell(-1, WIDTH + 1));
+    assertThrows(IndexOutOfBoundsException.class, () -> controller.placePiece(new PlacePieceCallback(-1, WIDTH + 1, PIECE_NAME)));
+    assertThrows(IndexOutOfBoundsException.class, () -> controller.clearCell(new ClearCellCallback(-1, WIDTH + 1)));
     assertThrows(IndexOutOfBoundsException.class, () -> controller.findPieceAt(-1, WIDTH + 1));
   }
 
   @Test
   void testPiecePlacement()
       throws OccupiedCellException, NullBoardException, ElementNotFoundException, MissingRequiredPropertyException, InvalidTypeException {
-    controller.makeBoard(WIDTH, HEIGHT);
+    controller.makeBoard(new MakeBoardCallback(WIDTH, HEIGHT));
     addPiece();
-    controller.placePiece(X, Y, PIECE_NAME);
+    controller.placePiece(new PlacePieceCallback(X, Y, PIECE_NAME));
     assertEquals(PIECE_NAME, controller.findPieceAt(X, Y));
   }
 
   @Test
   void testEmpty()
       throws OccupiedCellException, NullBoardException, ElementNotFoundException, MissingRequiredPropertyException, InvalidTypeException {
-    controller.makeBoard(WIDTH, HEIGHT);
+    controller.makeBoard(new MakeBoardCallback(WIDTH, HEIGHT));
     for (int i = 0; i < WIDTH; i++) {
       for (int j = 0; j < HEIGHT; j++) {
         assertEquals(EMPTY, controller.findPieceAt(i, j));
       }
     }
     addPiece();
-    controller.placePiece(X, Y, PIECE_NAME);
-    controller.clearCell(X, Y);
+    controller.placePiece(new PlacePieceCallback(X, Y, PIECE_NAME));
+    controller.clearCell(new ClearCellCallback(X, Y));
     assertEquals(EMPTY, controller.findPieceAt(X, Y));
   }
 
   @Test
   void testOccupiedCell()
       throws OccupiedCellException, NullBoardException, ElementNotFoundException, MissingRequiredPropertyException, InvalidTypeException {
-    controller.makeBoard(WIDTH, HEIGHT);
+    controller.makeBoard(new MakeBoardCallback(WIDTH, HEIGHT));
     addPiece();
-    controller.placePiece(X, Y, PIECE_NAME);
-    assertThrows(OccupiedCellException.class, () -> controller.placePiece(X, Y, PIECE_NAME));
+    controller.placePiece(new PlacePieceCallback(X, Y, PIECE_NAME));
+    assertThrows(OccupiedCellException.class, () -> controller.placePiece(new PlacePieceCallback(X, Y, PIECE_NAME)));
   }
 
   @Test
   void testSave()
       throws OccupiedCellException, NullBoardException, ElementNotFoundException, InvalidTypeException, MissingRequiredPropertyException {
-    controller.makeBoard(WIDTH, HEIGHT);
+    controller.makeBoard(new MakeBoardCallback(WIDTH, HEIGHT));
     addPiece();
     properties = new HashSet<>();
     properties.add(PropertyFactory.makeProperty(ACTIONS, ACTION_NAME));
     properties.add(PropertyFactory.makeProperty(CONDITIONS, CONDITION_NAME));
-    controller.update(RULE_TYPE, RULE_NAME, properties);
+    controller.update(new UpdateGameElementCallback(RULE_TYPE, RULE_NAME, properties));
     File file = new File(TEST_FILENAME);
-    controller.save(file);
+    controller.save(new SaveCallback(file));
   }
 
   @Test
   void testSaveException() {
     File file = new File(TEST_FILENAME);
-    assertThrows(NullBoardException.class, () -> controller.save(file));
-    controller.makeBoard(WIDTH, HEIGHT);
+    assertThrows(NullBoardException.class, () -> controller.save(new SaveCallback(file)));
+    controller.makeBoard(new MakeBoardCallback(WIDTH, HEIGHT));
   }
 
   @Test
@@ -171,14 +177,14 @@ public class ControllerTest extends DukeApplicationTest {
     properties.add(PropertyFactory.makeProperty(IMAGE, PIECE_IMAGE));
     properties.add(PropertyFactory.makeProperty(PLAYER, PIECE_PLAYER));
     properties.add(PropertyFactory.makeProperty(ID, PIECE_ID));
-    controller.update(PIECE_TYPE, PIECE_NAME, properties);
+    controller.update(new UpdateGameElementCallback(PIECE_TYPE, PIECE_NAME, properties));
   }
 
   private void addRule() throws MissingRequiredPropertyException, InvalidTypeException {
     properties = new HashSet<>();
     properties.add(PropertyFactory.makeProperty(ACTIONS, ACTION_NAME));
     properties.add(PropertyFactory.makeProperty(CONDITIONS, CONDITION_NAME));
-    controller.update(RULE_TYPE, RULE_NAME, properties);
+    controller.update(new UpdateGameElementCallback(RULE_TYPE, RULE_NAME, properties));
   }
 
 
