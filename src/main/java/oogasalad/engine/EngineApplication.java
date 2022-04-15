c66b7da0e538d906b16bb310b2bbd178a7db34d2
Cynthia France <cf219@duke.edu>
2022-04-14 23:33:57 -0400

    connect opening view and gameview
    resolved more merge conflicts

--- src/main/java/oogasalad/engine/EngineApplication.java
+++ src/main/java/oogasalad/engine/EngineApplication.java
@@ -19,47 +19,13 @@ public class EngineApplication extends Application {
   @Override
   public void start(Stage stage) throws Exception {
 
-//    Board board = GameParser.readInitialBoard(Constants.CHECKERS_FILE);
-//
-//    BoardView boardView = new BoardView(board.getHeight(), board.getWidth(), 350, 350);
-//
-//    Controller controller = new Controller(board);
-//
-//    boardView.addController(controller);
-//    Group root = new Group();
-//    root.getChildren().add(boardView.getRoot());
-
     ViewManager manager = new ViewManager(stage);
 
-    //Scene scene = manager.createGameView(boardView, controller).makeScene();
-    //Scene scene = manager.createOpeningView().makeScene();
     Scene scene = manager.getCurrScene();
-    scene.getStylesheets().add(getClass().getResource("/css/engine.css").toExternalForm());
+    //scene.getStylesheets().add(getClass().getResource("/css/engine.css").toExternalForm());
 
     stage.setTitle("OOGABOOGA Engine");
     stage.setScene(scene);
     stage.show();
   }
-
-//  public void start(Stage stage) throws Exception {
-//
-//    Board board = GameParser.readInitialBoard(Constants.CHECKERS_FILE);
-//
-//    BoardView boardView = new BoardView(board.getHeight(), board.getWidth(), 350, 350);
-//
-//    Controller controller = new Controller(board);
-//
-//    boardView.addController(controller);
-//    Group root = new Group();
-//    root.getChildren().add(boardView.getRoot());
-//
-//    ViewManager manager = new ViewManager();
-//
-//    Scene scene = manager.createGameView(boardView, controller).makeScene();
-//    scene.getStylesheets().add(getClass().getResource("/css/engine.css").toExternalForm());
-//
-//    stage.setTitle("OOGABOOGA Engine");
-//    stage.setScene(scene);
-//    stage.show();
-//  }
 }
