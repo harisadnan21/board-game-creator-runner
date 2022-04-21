833e01a85492cee78bbbd59c7535116392934ede
Mike Keohane <mbk28@duke.edu>
2022-04-21 18:52:51 -0400

    made basic structure for help page. Should just be reformatting and filling out property files now

--- src/main/java/oogasalad/builder/view/tab/HelpTab.java
+++ src/main/java/oogasalad/builder/view/tab/HelpTab.java
@@ -2,15 +2,18 @@ package oogasalad.builder.view.tab;
 
 import static oogasalad.builder.view.tab.boardTab.BoardTab.BOARD_TYPE;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.ResourceBundle;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import oogasalad.builder.model.property.Property;
 import oogasalad.builder.view.ViewResourcesSingleton;
 import oogasalad.builder.view.callback.CallbackDispatcher;
 import oogasalad.builder.view.callback.GetElementNamesCallback;
+import oogasalad.builder.view.callback.GetPropertiesCallback;
 
 
 public class HelpTab extends BasicTab{
@@ -44,12 +47,19 @@ public class HelpTab extends BasicTab{
 
   public void displayHelpForElement(String type){
     leftDisplay.getChildren().clear();
-    if (type.equals(BOARD_TYPE)){
-      leftDisplay.getChildren().add(new Text(ViewResourcesSingleton.getInstance().getString(type + "-" + HELP)));
+    leftDisplay.getChildren().add(new Text(ViewResourcesSingleton.getInstance().getString(type + "-" + HELP)));
+
+    if (getCallbackDispatcher().call(new GetPropertiesCallback(type)).isPresent()){
+      Collection<Property> elementProperties = getCallbackDispatcher().call(new GetPropertiesCallback(type)).orElseThrow();
+      for (Property property : elementProperties){
+        String propertyName = property.name();
+        if (propertyName.contains("required-")){
+          propertyName = propertyName.replace("required", type);
+        }
+        leftDisplay.getChildren().add(new Text(ViewResourcesSingleton.getInstance().getString(propertyName + "-" + HELP)));
+      }
     }
-    else{
-      Collection<String> elementPropertyNames = getCallbackDispatcher().call(new GetElementNamesCallback(type)).orElseThrow();
-    }
+
   }
   @Override
   public void loadElements() {
