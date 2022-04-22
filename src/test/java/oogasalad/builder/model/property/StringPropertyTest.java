9ddf885a9280e5a6985c50ff589ff7d3a5bc1c50
Ricky Weerts <rrw24@duke.edu>
2022-04-22 07:22:50 -0400

    Improved builder property tests

--- src/test/java/oogasalad/builder/model/property/StringPropertyTest.java
+++ src/test/java/oogasalad/builder/model/property/StringPropertyTest.java
@@ -1,10 +1,10 @@
 package oogasalad.builder.model.property;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test for String Property Class
@@ -15,6 +15,7 @@ public class StringPropertyTest {
 
   private static final String PROPERTY_NAME = "propName";
   private static final String PROPERTY_VALUE = "value";
+  private static final String DIFFERENT_VALUE = "Different value";
   private static final String PROPERTY_FORM = "form";
 
   @Test
@@ -29,6 +30,10 @@ public class StringPropertyTest {
   void testWith() {
     Property property = new StringProperty(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
     Property newProperty = property.with(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
+    Property withChangedValue = property.with(DIFFERENT_VALUE);
+    assertEquals(property, withChangedValue);
+    assertFalse(property.fullEquals(withChangedValue));
+    assertEquals(withChangedValue.valueAsString(), DIFFERENT_VALUE);
     assertEquals(property, newProperty);
   }
 
@@ -36,12 +41,35 @@ public class StringPropertyTest {
   void testEquality() {
     Property property1 = new StringProperty(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
     Property property2 = new StringProperty(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
+    Property property3 = new StringProperty("Different name", PROPERTY_VALUE, PROPERTY_FORM);
+    Property property4 = new StringProperty(PROPERTY_NAME, DIFFERENT_VALUE, PROPERTY_FORM);
     assertEquals(property1, property2);
+    assertEquals(property1, property4);
+    assertNotEquals(property1, property3);
   }
 
   @Test
   void testHashCode() {
-    Property property = new StringProperty(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
-    int code = property.hashCode();
+    Property property1 = new StringProperty(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
+    Property property2 = new StringProperty(PROPERTY_NAME, PROPERTY_VALUE, "Different form");
+    assertEquals(property1.hashCode(), property2.hashCode());
+  }
+
+  @Test
+  void testFullEquals() {
+    Property property1 = new StringProperty(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
+    Property property2 = new StringProperty(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
+    Property property3 = new StringProperty(PROPERTY_NAME, DIFFERENT_VALUE, PROPERTY_FORM);
+    assertTrue(property1.fullEquals(property2));
+    assertFalse(property1.fullEquals(property3));
+  }
+
+  @Test
+  void testShortName() {
+    Property property1 = new StringProperty("required-some-namespace-here-" + PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
+    Property property2 = new StringProperty(PROPERTY_NAME, PROPERTY_VALUE, PROPERTY_FORM);
+    assertEquals(property1.shortName(), PROPERTY_NAME);
+    assertEquals(property2.shortName(), PROPERTY_NAME);
+
   }
 }
