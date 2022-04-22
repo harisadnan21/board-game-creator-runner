6d19b366aa80000a9ae538262b68e7d68d4897de
Ricky Weerts <rrw24@duke.edu>
2022-04-22 15:10:41 -0400

    Moved builder error handling to cover the entire program

--- src/main/java/oogasalad/Main.java
+++ src/main/java/oogasalad/Main.java
@@ -3,8 +3,12 @@ package oogasalad;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import oogasalad.builder.BuilderMain;
 import oogasalad.view.OpeningSplashScreen;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Feel free to completely change this code or delete it entirely. 
@@ -13,10 +17,20 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        setupErrorHandling();
         OpeningSplashScreen opening = new OpeningSplashScreen(stage);
         Scene scene = opening.makeScene();
         stage.setTitle("OOGABOOGA");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void setupErrorHandling() {
+        final Logger backupLogger = LogManager.getLogger(Main.class);
+        Thread.currentThread().setUncaughtExceptionHandler((thread, throwable) -> {
+            Logger logger = throwable.getStackTrace().length > 0 ? LogManager.getLogger(throwable.getStackTrace()[0].getClassName()) : backupLogger;
+            logger.throwing(throwable);
+            new Alert(Alert.AlertType.ERROR, throwable.getMessage()).showAndWait();
+        });
     }
 }
