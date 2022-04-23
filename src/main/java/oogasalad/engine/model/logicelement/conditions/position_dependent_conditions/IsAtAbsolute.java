e2fb68c8f04da72f50bcc31c61ddfa4253df9456
Jake Heller <jake.heller@duke.edu>
2022-04-23 02:54:16 -0400

    add conditions for checking if relative point is at absolute point

---
+++ src/main/java/oogasalad/engine/model/logicelement/conditions/position_dependent_conditions/IsAtAbsolute.java
@@ -0,0 +1,29 @@
+package oogasalad.engine.model.logicelement.conditions.position_dependent_conditions;
+
+import javafx.geometry.Pos;
+import oogasalad.engine.model.board.Board;
+import oogasalad.engine.model.board.Position;
+import oogasalad.engine.model.logicelement.conditions.Condition;
+
+/**
+ * Condition checks if relative point is equal to absolute point
+ * @author Jake Heller
+ */
+public class IsAtAbsolute extends Condition {
+
+  /**
+   *
+   * @param parameters size-4 array [relativeRow, relativeColumn, absoluteRow, absoluteColumn]
+   */
+  public IsAtAbsolute(int[] parameters) {
+    super(parameters);
+  }
+
+  @Override
+  public boolean isTrue(Board board, Position referencePoint) {
+    Position relativePoint = new Position(myParameters[0], myParameters[1]);
+    Position absolutePoint = new Position(myParameters[2], myParameters[3]);
+    relativePoint = transformToRelative(referencePoint, relativePoint);
+    return relativePoint.equals(absolutePoint);
+  }
+}
