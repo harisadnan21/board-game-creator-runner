97c982ce80e64a4261be2803cc8e37a2f151f5b1
Thivya Sivarajah <ts346@duke.edu>
2022-04-22 22:06:45 -0400

    new additions to the format tab

--- src/main/java/oogasalad/builder/view/tab/FormatTab.java
+++ src/main/java/oogasalad/builder/view/tab/FormatTab.java
@@ -5,7 +5,11 @@ import static oogasalad.builder.view.tab.boardTab.BoardTab.BOARD_TYPE;
 import javafx.scene.Node;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import oogasalad.builder.view.callback.CallbackDispatcher;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.ResourceBundle;
 import javafx.scene.Node;
@@ -24,18 +28,21 @@ public class FormatTab extends AbstractTab{
     public static String ELEMENTS_PACKAGE = "FormatList";
     private StackPane leftDisplay;
     private VBox rightHelpBox;
-    private String[] elementNames = {"tabFormat", "Sunset", "DukevUNC"};
-    //private static final ResourceBundle elementNames = ResourceBundle.getBundle(HELP_RESOURCE_PATH + ELEMENTS_PACKAGE);
+    private static final ResourceBundle elementNames = ResourceBundle.getBundle(HELP_RESOURCE_PATH + ELEMENTS_PACKAGE);
 
     public FormatTab(CallbackDispatcher dispatcher) { super(FORMAT, dispatcher);}
 
     @Override
     protected Node setupRightSide() {
         rightHelpBox = new VBox();
-//        for (String name : elementNames){
-//            rightHelpBox.getChildren().add(makeButton(name, e -> displayChangeBackground()));
-//        }
-        //rightHelpBox.getStyleClass().add("rightPane");
+
+        rightHelpBox.getChildren().add(makeButton("tabFormat-format", e -> displayChangeBackground()));
+        rightHelpBox.getChildren().add(makeButton("sunset-format", e -> displayChangeBackground()));
+        rightHelpBox.getChildren().add(makeButton("DukevUNC-format", e -> displayChangeBackground()));
+        rightHelpBox.getChildren().add(makeButton("normalmode-format", e -> displayChangeBackground()));
+        rightHelpBox.getChildren().add(makeButton("presentationmode-format", e -> displayChangeBackground()));
+        rightHelpBox.getChildren().add(makeButton("fancymode-format", e -> displayChangeBackground()));
+        rightHelpBox.getStyleClass().add("rightPane");
         return rightHelpBox;
         //return null;
     }
