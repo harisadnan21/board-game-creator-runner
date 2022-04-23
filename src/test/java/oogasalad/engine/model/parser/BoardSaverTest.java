98dec32fd019b509144a0fda4a4bc456ba541a3c
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-04-22 21:35:00 -0400

    Updated board saver tests

--- src/test/java/oogasalad/engine/model/parser/BoardSaverTest.java
+++ src/test/java/oogasalad/engine/model/parser/BoardSaverTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import oogasalad.engine.model.board.Board;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,23 +13,24 @@ import org.junit.jupiter.api.Test;
 public class BoardSaverTest {
 
   private static final String TEST_PARSE_FILENAME = "data/tests/parser/test.json";
-  private static final String TEST_SAVE_FILENAME = "data/tests/parser/saver/test.json";
+  private static final String TEST_PARSE_DIRECTORY = "data/tests/parser/";
+  private static final String TEST_SAVE_DIRECTORY = "data/tests/parser/saver/";
 
   private BoardSaver saver;
   private BoardParser parser;
 
   @BeforeEach
   public void setUp() {
-    saver = new BoardSaver(new File(TEST_SAVE_FILENAME));
+    saver = new BoardSaver(new File(TEST_PARSE_DIRECTORY));
     parser = new BoardParser();
   }
 
   @Test
-  void testSaveModifiedBoard() throws FileNotFoundException {
+  void testSaveModifiedBoard() throws IOException {
     File file = new File(TEST_PARSE_FILENAME);
     Board board = parser.parse(file);
     Board newBoard = board.placeNewPiece(0,0,2,1);
-    saver.saveBoardConfig(newBoard);
+    saver.saveBoardConfig(newBoard, new File(TEST_SAVE_DIRECTORY));
   }
 
 }
