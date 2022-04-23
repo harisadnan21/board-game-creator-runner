f5ee023e811d9801724f0852aba87ff92016a3c0
Alex Bildner <alexander.bildner@duke.edu>
2022-04-23 14:28:04 -0400

    added equals and hashcode tests for pattern

--- src/test/java/oogasalad/engine/model/ai/evaluation/patterns/PatternTest.java
+++ src/test/java/oogasalad/engine/model/ai/evaluation/patterns/PatternTest.java
@@ -81,10 +81,37 @@ class PatternTest {
 
   @Test
   void testEquals1() {
+
+    Pattern pattern1a = new Pattern(List.of(new PositionState(new Position(0,0), new Piece(1, Piece.PLAYER_ONE))));
+    Pattern pattern1b = new Pattern(List.of(new PositionState(new Position(0,0), new Piece(1, Piece.PLAYER_ONE))));
+    assertEquals(pattern1a, pattern1b);
+
+    Pattern pattern2a = new Pattern(List.of(new PositionState(new Position(0,0), new Piece(1, Piece.PLAYER_TWO))));
+    Pattern pattern2b = new Pattern(List.of(new PositionState(new Position(0,0), new Piece(1, Piece.PLAYER_TWO))));
+    assertEquals(pattern2a, pattern2b);
+
+    assertNotEquals(pattern1a, pattern2a);
+    assertNotEquals(pattern1a, pattern2b);
+
+    assertNotEquals(pattern1b, pattern2a);
+    assertNotEquals(pattern1b, pattern2b);
   }
 
   @Test
   void testHashCode1() {
+    Pattern pattern1a = new Pattern(List.of(new PositionState(new Position(0,0), new Piece(1, Piece.PLAYER_ONE))));
+    Pattern pattern1b = new Pattern(List.of(new PositionState(new Position(0,0), new Piece(1, Piece.PLAYER_ONE))));
+    assertEquals(pattern1a.hashCode(), pattern1b.hashCode());
+
+    Pattern pattern2a = new Pattern(List.of(new PositionState(new Position(0,0), new Piece(1, Piece.PLAYER_TWO))));
+    Pattern pattern2b = new Pattern(List.of(new PositionState(new Position(0,0), new Piece(1, Piece.PLAYER_TWO))));
+    assertEquals(pattern2a.hashCode(), pattern2b.hashCode());
+
+    assertNotEquals(pattern1a.hashCode(), pattern2a.hashCode());
+    assertNotEquals(pattern1a.hashCode(), pattern2b.hashCode());
+
+    assertNotEquals(pattern1b.hashCode(), pattern2a.hashCode());
+    assertNotEquals(pattern1b.hashCode(), pattern2b.hashCode());
   }
 
   @Test
