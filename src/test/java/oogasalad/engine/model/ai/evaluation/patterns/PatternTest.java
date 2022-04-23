6a9dc22daa810c48d001893c25c99b81518b2c3d
Alex Bildner <alexander.bildner@duke.edu>
2022-04-23 14:22:40 -0400

    filled in missing part of pattern class and began adding tests

--- src/test/java/oogasalad/engine/model/ai/evaluation/patterns/PatternTest.java
+++ src/test/java/oogasalad/engine/model/ai/evaluation/patterns/PatternTest.java
@@ -2,6 +2,11 @@ package oogasalad.engine.model.ai.evaluation.patterns;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Collection;
+import java.util.List;
+import oogasalad.engine.model.board.Piece;
+import oogasalad.engine.model.board.Position;
+import oogasalad.engine.model.board.PositionState;
 import org.junit.jupiter.api.Test;
 
 class PatternTest {
@@ -44,5 +49,49 @@ class PatternTest {
 
   @Test
   void testToString() {
+  }
+
+  @Test
+  void testIncludes1() {
+  }
+
+  @Test
+  void pieceAt() {
+  }
+
+  @Test
+  void testGetPositionStates() {
+  }
+
+  @Test
+  void testGetPositions() {
+  }
+
+  @Test
+  void testGetPieceAtPositions() {
+  }
+
+  @Test
+  void testGetSize() {
+  }
+
+  @Test
+  void compareTo() {
+  }
+
+  @Test
+  void testEquals1() {
+  }
+
+  @Test
+  void testHashCode1() {
+  }
+
+  @Test
+  void forPlayer() {
+    Pattern pattern1 = new Pattern(List.of(new PositionState(new Position(0,0), new Piece(1, Piece.PLAYER_ONE))));
+    Pattern pattern2 = new Pattern(List.of(new PositionState(new Position(0,0), new Piece(1, Piece.PLAYER_TWO))));
+    assertEquals(Piece.PLAYER_ONE, pattern1.forPlayer());
+    assertEquals(Piece.PLAYER_TWO, pattern2.forPlayer());
   }
 }
