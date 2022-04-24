834fc9bb17c7967df2734424b8e0e809d5d28dcb
Robert Cranston <rvc6@duke.edu>
2022-04-23 22:21:56 -0400

    error checking and n in a row

--- src/main/java/oogasalad/engine/model/logicelement/conditions/position_independent_conditions/NInARow.java
+++ src/main/java/oogasalad/engine/model/logicelement/conditions/position_independent_conditions/NInARow.java
@@ -1,5 +1,6 @@
 package oogasalad.engine.model.logicelement.conditions.position_independent_conditions;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 import oogasalad.engine.model.board.Board;
@@ -10,7 +11,6 @@ import oogasalad.engine.model.board.PositionState;
 
 public class NInARow extends BoardCondition{
   private int currentPlayer = -100;
-  private int n;
   public NInARow(int[] parameters){
     super(parameters);
   }
@@ -23,7 +23,7 @@ public class NInARow extends BoardCondition{
    */
   @Override
   public boolean isTrue(Board board, Position referencePoint) {
-    return checkForHorizontal(board) || checkForVertical(board);
+    return checkForHorizontal(board) || checkForVertical(board) || checkForDiagonal(board);
   }
 
 
@@ -37,6 +37,14 @@ public class NInARow extends BoardCondition{
     return anyHaveNInARow(cols);
 
   }
+  private boolean checkForDiagonal(Board board){
+    for(PositionState state : board){
+      if(nInARow(createLine(state.position(), board, -1)) || nInARow(createLine(state.position(), board, 1))){
+        return true;
+      }
+    }
+    return false;
+  }
 
   private boolean anyHaveNInARow(Stream<List<PositionState>> positionStates) {
     return positionStates.anyMatch(this::nInARow);
@@ -46,11 +54,70 @@ public class NInARow extends BoardCondition{
 
     int count = 0;
     for(PositionState positionState : positionStates) {
-      count = positionState.player()==currentPlayer ? count+1 : 0;
+      count = positionState.player()==currentPlayer ? count+1 : (positionState.isEmpty() ? 0 : 1);
       currentPlayer = positionState.isEmpty() ? -100 : positionState.player();
-      if (count == n) { return true; }
+      if (count == myParameters[0]) { return true; }
     }
     return false;
   }
+  private List<PositionState> createLine(Position start, Board board, int direction){
+    List<PositionState> line = new ArrayList<>();
+    int row = start.row();
+    int col = start.column();
+    for(int i = 0; i<myParameters[0]; i++){
+      if(board.isValidPosition(row, col)) {
+        line.add(board.getPositionStateAt(row, col));
+        row= row + direction;
+        col= col + direction;
+      }
+    }
+    return line;
+  }
+
+
+//  private boolean checkForHorizontal(Board board) {
+//    n = 0;
+//    for(int row = 0; row < board.getHeight(); row++){
+//      for(int col = 0; col < board.getWidth(); col++){
+//        if (checkCurrentCell(board, row, col)) {
+//          return true;
+//        }
+//      }
+//      n = 0;
+//    }
+//    return false;
+//  }
+//
+//  //check for n pieces of the same type in a vertical row
+//  private boolean checkForVertical(Board board) {
+//    n = 0;
+//    for(int col = 0; col < board.getWidth(); col++){
+//      for(int row = 0; row < board.getHeight(); row++){
+//        if (checkCurrentCell(board, row, col)) {
+//          return true;
+//        }
+//      }
+//      n = 0;
+//    }
+//    return false;
+//  }
+//
+//  private boolean checkCurrentCell(Board board, int row, int col) {
+//    if(board.getPositionStateAt(row, col).piece() != Piece.EMPTY) {
+//      if (board.getPositionStateAt(row, col).player() == currentPlayer) {
+//        n++;
+//      } else {
+//        currentPlayer = board.getPositionStateAt(row, col).player();
+//        n = 1;
+//      }
+//    }
+//    else{
+//      n = 0;
+//    }
+//    return n == myParameters[0];
+//  }
+//
+//}
+
 
 }
