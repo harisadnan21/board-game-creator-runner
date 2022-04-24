236196da5b73e7957c85957b243a8b12b9c7d242
Alex Bildner <alexander.bildner@duke.edu>
2022-04-23 20:06:48 -0400

    added AI factory

---
+++ src/main/java/oogasalad/engine/model/player/AIPlayerFactory.java
@@ -0,0 +1,20 @@
+package oogasalad.engine.model.player;
+
+import java.util.Collection;
+import oogasalad.engine.model.ai.AIOracle;
+import oogasalad.engine.model.ai.AIPlayer;
+import oogasalad.engine.model.ai.SelectorFactory;
+import oogasalad.engine.model.ai.enums.Difficulty;
+import oogasalad.engine.model.ai.enums.WinType;
+import oogasalad.engine.model.ai.evaluation.patterns.Pattern;
+import oogasalad.engine.model.ai.moveSelection.Selects;
+import oogasalad.engine.model.player.AbstractPlayer;
+
+public class AIPlayerFactory {
+
+  public static AbstractPlayer makeAIPlayer(Difficulty difficulty, WinType winType, int playerNumber, AIOracle aiOracle, Collection<Pattern> patterns) {
+    Selects selects = SelectorFactory.makeSelector(difficulty, winType, playerNumber, aiOracle, patterns);
+    return new AIPlayer(playerNumber, selects);
+  }
+
+}
