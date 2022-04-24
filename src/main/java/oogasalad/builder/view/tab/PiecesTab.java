6c8287132dd571b3a7ee9b22cc5a0547e05b4c85
Mike Keohane <mbk28@duke.edu>
2022-04-23 23:11:06 -0400

    reformatted and commented pieces tab

--- src/main/java/oogasalad/builder/view/tab/PiecesTab.java
+++ src/main/java/oogasalad/builder/view/tab/PiecesTab.java
@@ -4,12 +4,20 @@ package oogasalad.builder.view.tab;
 import oogasalad.builder.view.callback.CallbackDispatcher;
 
 /**
- * 
+ * Represents the Pieces Tab
+ *
+ * @author Ricky Weerts
  */
 public class PiecesTab extends GameElementTab {
-    public static String PIECE = "piece";
 
-    public PiecesTab(CallbackDispatcher dispatcher) {
-        super(dispatcher, PIECE);
-    }
+  public static String PIECE = "piece";
+
+  /**
+   * Initializes the PiecesTab by calling the GameElementTab super class
+   *
+   * @param dispatcher - callback dispatcher to communicate with the controller
+   */
+  public PiecesTab(CallbackDispatcher dispatcher) {
+    super(dispatcher, PIECE);
+  }
 }
