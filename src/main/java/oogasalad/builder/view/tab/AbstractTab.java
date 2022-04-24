d0128c60358e6a96cfcb38b3d6bb7837a90ae69b
Mike Keohane <mbk28@duke.edu>
2022-04-23 22:21:21 -0400

    commented and reformatted AbstractTab

--- src/main/java/oogasalad/builder/view/tab/AbstractTab.java
+++ src/main/java/oogasalad/builder/view/tab/AbstractTab.java
@@ -10,6 +10,8 @@ import oogasalad.builder.view.ViewResourcesSingleton;
 import oogasalad.builder.view.callback.CallbackDispatcher;
 
 /**
+ * The Basic Tab which extends BorderPane is the basis for all of the tabs
+ *
  * @author Mike Keohane
  */
 public abstract class AbstractTab extends BorderPane implements BasicTab {
@@ -19,8 +21,12 @@ public abstract class AbstractTab extends BorderPane implements BasicTab {
   private SplitPane splitPane;
   private CallbackDispatcher callbackDispatcher;
 
-
-
+  /**
+   * Initializes the tab by setting up the title and SplitPane
+   *
+   * @param type       - type of the tab being created, correlated to model
+   * @param dispatcher - callback dispatcher to communicate to controller.
+   */
   public AbstractTab(String type, CallbackDispatcher dispatcher) {
     this.type = type;
     this.callbackDispatcher = dispatcher;
@@ -37,25 +43,64 @@ public abstract class AbstractTab extends BorderPane implements BasicTab {
     splitPane.setDividerPositions(BASIC_TAB_DIVIDER_POSITION);
     setCenter(splitPane);
   }
+
+  /**
+   * Creates a button with specified name and event on press
+   *
+   * @param property - key which correlates to text in the resources file
+   * @param handler  - action which the button performs
+   * @return - the created button
+   */
   protected Button makeButton(String property, EventHandler<ActionEvent> handler) {
     Button result = new Button();
     result.setText(ViewResourcesSingleton.getInstance().getString(property));
     result.setOnAction(handler);
     return result;
   }
-  protected SplitPane getSplitPane(){
+
+  /**
+   * returns the SplitPane used in the tab
+   *
+   * @return SplitPane splitPane
+   */
+  protected SplitPane getSplitPane() {
     return splitPane;
   }
-  protected String getType(){
+
+  /**
+   * returns the type of the tab
+   *
+   * @return String type
+   */
+  protected String getType() {
     return type;
   }
 
-  protected CallbackDispatcher getCallbackDispatcher(){
+  /**
+   * returns the callbackDispatcher
+   *
+   * @return CallbackDispatcher callbackDispatcher
+   */
+  protected CallbackDispatcher getCallbackDispatcher() {
     return callbackDispatcher;
   }
+
+  /**
+   * returns a node to set the right side of the SplitPane
+   *
+   * @return Node to set right pane
+   */
   protected abstract Node setupRightSide();
 
+  /**
+   * returns a node to set the left side of the SplitPane
+   *
+   * @return Node to set right pane
+   */
   protected abstract Node setupLeftSide();
 
+  /**
+   * Loads the elements of a tab based on the model
+   */
   public abstract void loadElements();
 }
