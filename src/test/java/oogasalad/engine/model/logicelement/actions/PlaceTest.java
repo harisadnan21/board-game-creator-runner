0ac5d50b9d9d89b053b3ce7622cd21edf509ed5b
Haris Adnan <haris.adnan@duke.edu>
2022-04-24 19:10:08 -0400

    completed tests for actions in logicelement

---
+++ src/test/java/oogasalad/engine/model/logicelement/actions/PlaceTest.java
@@ -0,0 +1,33 @@
+package oogasalad.engine.model.logicelement.actions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import oogasalad.engine.model.board.Board;
+import oogasalad.engine.model.board.cells.Position;
+import oogasalad.engine.model.board.cells.PositionState;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class that tests the Place class
+ * @author haris adnan
+ */
+class PlaceTest {
+  PositionState[][] positionStates = new PositionState[4][4];
+  Board TestBoard = new Board(positionStates);
+
+
+  /**
+   * test for the execute method for the Place class
+   */
+  @Test
+  void execute() {
+    assertEquals(TestBoard.getPiece(2,2).player(), -1);
+    assertEquals(TestBoard.getPiece(2,2).type(), -1);
+    int[] paramarray = new int[]{1,1,1,1,1};
+    Place PlaceAction= new Place(paramarray);
+    Position position = new Position(2,2);
+    Board newTestBoard = PlaceAction.execute(TestBoard, position);
+    assertEquals(newTestBoard.getPiece(2,2).type(), -1);
+    assertEquals(newTestBoard.getPiece(2,2).player(), -1);
+  }
+}
