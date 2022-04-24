1e32ac85927ab81b17e93e3fe2366b03809c4897
Mike Keohane <mbk28@duke.edu>
2022-04-24 13:19:36 -0400

    comment and reformat

--- src/main/java/oogasalad/builder/view/FormatDropDown.java
+++ src/main/java/oogasalad/builder/view/FormatDropDown.java
@@ -6,23 +6,37 @@ import java.util.Enumeration;
 import java.util.ResourceBundle;
 import javafx.scene.control.ComboBox;
 
+/**
+ * Class that extends ComboBox and contains the options to change the view format
+ *
+ * @author Mike Keohane
+ */
 public class FormatDropDown extends ComboBox<String> {
+
   public static final String STYLE_OPTIONS_PATH = "FormatOptions";
   ResourceBundle styleOptions;
 
-  public FormatDropDown(BuilderView view){
+  /**
+   * Constructs the drop-down and sets the ChangeListener to call view.setFormat()
+   *
+   * @param view - BuilderView instance
+   */
+  public FormatDropDown(BuilderView view) {
     fillDropDown();
     this.setPromptText(ViewResourcesSingleton.getInstance().getString("formatPrompt"));
-    this.valueProperty().addListener( (observable, oldValue, newValue) -> view.setFormat(
-        styleOptions.getString(ViewResourcesSingleton.getInstance().getString(newValue.replace(" ", "_")))));
+    this.valueProperty().addListener((observable, oldValue, newValue) -> view.setFormat(
+        styleOptions.getString(
+            ViewResourcesSingleton.getInstance().getString(newValue.replace(" ", "_")))));
   }
 
-  private void fillDropDown(){
+  //fills DropDown from a file
+  private void fillDropDown() {
     styleOptions = ResourceBundle.getBundle(
         DEFAULT_PROPERTY_PACKAGE + STYLE_OPTIONS_PATH);
     Enumeration keys = styleOptions.getKeys();
-    while (keys.asIterator().hasNext()){
-      this.getItems().add(ViewResourcesSingleton.getInstance().getString((String) keys.nextElement()));
+    while (keys.asIterator().hasNext()) {
+      this.getItems()
+          .add(ViewResourcesSingleton.getInstance().getString((String) keys.nextElement()));
     }
   }
 }
