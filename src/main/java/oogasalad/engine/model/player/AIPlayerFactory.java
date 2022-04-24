2088684c7dcbd43389022384ba1d05a1394d87af
Alex Bildner <alexander.bildner@duke.edu>
2022-04-23 20:34:21 -0400

    added javadocs

--- src/main/java/oogasalad/engine/model/player/AIPlayerFactory.java
+++ src/main/java/oogasalad/engine/model/player/AIPlayerFactory.java
@@ -10,8 +10,24 @@ import oogasalad.engine.model.ai.evaluation.patterns.Pattern;
 import oogasalad.engine.model.ai.moveSelection.Selects;
 import oogasalad.engine.model.player.AbstractPlayer;
 
+/**
+ * An AI Player Factory which can be used to create AI players to play against humans. This factory
+ * is the only necessary interface between the AI and the rest of the program needed to directly
+ * create a player
+ * @author Alex
+ */
 public class AIPlayerFactory {
 
+  /**
+   * Makes an AI Player
+   *
+   * @param difficulty   the chosen difficulty
+   * @param winType      the win type of the game - total pieces or pattern
+   * @param playerNumber the player number of the AI
+   * @param aiOracle     the injected oracle which tells the AI what moves are available and their results
+   * @param patterns     the patterns which can win a game for games with pattern based winning
+   * @return the AI Player
+   */
   public AbstractPlayer makeAIPlayer(Difficulty difficulty, WinType winType, int playerNumber, AIOracle aiOracle, Collection<Pattern> patterns) {
     Selects selects = SelectorFactory.makeSelector(difficulty, winType, playerNumber, aiOracle, patterns);
     return new AIPlayer(playerNumber, selects);
