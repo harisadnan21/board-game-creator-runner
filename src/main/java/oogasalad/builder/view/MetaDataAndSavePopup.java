9506c5c04c7959cdac926e5b09f87afa18972aa5
Mike Keohane <mbk28@duke.edu>
2022-04-23 23:33:55 -0400

    commented and reformatted MetaDataAndSavePopup

--- src/main/java/oogasalad/builder/view/MetaDataAndSavePopup.java
+++ src/main/java/oogasalad/builder/view/MetaDataAndSavePopup.java
@@ -22,9 +22,11 @@ import oogasalad.builder.view.property.PropertyEditor;
 
 /**
  * Creates a popup for the user to edit the metadata then save the file
+ *
  * @author Mike Keohane
  */
 public class MetaDataAndSavePopup {
+
   private static final String SAVE_CHOOSER_TITLE_KEY = "SaveChooserTitle";
   public static final String METADATA = "metadata";
   public static final String TITLE = "Title";
@@ -32,51 +34,61 @@ public class MetaDataAndSavePopup {
   private VBox metaBox;
   private PropertyEditor propertyEditor;
   private Stage metaStage;
-  public MetaDataAndSavePopup(CallbackDispatcher callbackDispatcher){
+
+  /**
+   * Initializes the popup by creating a state and propertyEditor
+   *
+   * @param callbackDispatcher - CallbackDispatcher to communicate with the controller
+   */
+  public MetaDataAndSavePopup(CallbackDispatcher callbackDispatcher) {
     this.callbackDispatcher = callbackDispatcher;
     propertyEditor = new PropertyEditor(callbackDispatcher);
     metaStage = new Stage();
     setupMetaDataPopup();
   }
 
-  private void setupMetaBox(){
+  private void setupMetaBox() {
     metaBox = new VBox();
     metaBox.getChildren().addAll(propertyEditor, makeButton("save", e -> saveAndCloseMeta()));
     metaBox.getStyleClass().add("rightPane");
   }
 
 
-  private void setupMetaDataPopup(){
+  private void setupMetaDataPopup() {
     setupMetaBox();
     metaStage.setTitle(ViewResourcesSingleton.getInstance().getString(METADATA + TITLE));
     Scene metaScene = new Scene(metaBox, 600, 600);
     metaScene.getStylesheets()
         .add(getClass().getResource(DEFAULT_RESOURCE_PACKAGE + TAB_FORMAT).toExternalForm());
     metaStage.setScene(metaScene);
-    Collection<Property> metaProperties = callbackDispatcher.call(new GetPropertiesCallback(METADATA)).orElseThrow();
+    Collection<Property> metaProperties = callbackDispatcher.call(
+        new GetPropertiesCallback(METADATA)).orElseThrow();
     propertyEditor.setElementPropertyTypeChoice(metaProperties);
     populateProperties();
     metaStage.show();
   }
-  private void populateProperties(){
-    if (callbackDispatcher.call(new GetElementNamesCallback(METADATA)).orElseThrow().contains(METADATA)){
+
+  private void populateProperties() {
+    if (callbackDispatcher.call(new GetElementNamesCallback(METADATA)).orElseThrow()
+        .contains(METADATA)) {
       propertyEditor.setElementProperties(
           callbackDispatcher.call(new GetElementPropertiesCallback(METADATA, METADATA))
               .orElseThrow());
     }
   }
 
-  private void saveAndCloseMeta(){
+  private void saveAndCloseMeta() {
     callbackDispatcher.call(new UpdateGameElementCallback(METADATA, METADATA,
-          propertyEditor.getElementProperties()));
+        propertyEditor.getElementProperties()));
     metaStage.close();
     goToSave();
   }
 
-  private void goToSave(){
+  private void goToSave() {
     Stage saveStage = new Stage();
     DirectoryChooser directoryChooser = new DirectoryChooser();
-    directoryChooser.setTitle(ViewResourcesSingleton.getInstance().getString(SAVE_CHOOSER_TITLE_KEY));
+    directoryChooser.setTitle(
+        ViewResourcesSingleton.getInstance().getString(SAVE_CHOOSER_TITLE_KEY));
     callbackDispatcher.call(new SaveCallback(directoryChooser.showDialog(saveStage)));
   }
 
