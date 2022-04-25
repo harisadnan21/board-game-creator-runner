3683b4189d5737485de077b85061d52108b6d330
Haris Adnan <haris.adnan@duke.edu>
2022-04-25 02:49:52 -0400

    completed position independant condition tests

---
+++ src/test/java/oogasalad/engine/model/logicelement/conditions/position_independent_conditions/NoMovesLeftTest.java
@@ -0,0 +1,18 @@
+package oogasalad.engine.model.logicelement.conditions.position_independent_conditions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+/**
+ * Class that tests the NoMovesLeft Condition
+ * @author Haris Adnan
+ */
+class NoMovesLeftTest {
+  /**
+   * Function that checks if the isTrue function works for the NoMovesCondition Condition.
+   */
+  @Test
+  void isTrue() {
+
+  }
+}
