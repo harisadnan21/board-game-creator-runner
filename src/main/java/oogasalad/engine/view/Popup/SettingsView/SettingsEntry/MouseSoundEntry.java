e459670809dade7854444954c7e8640b4d13ef4b
Cynthia France <cf219@duke.edu>
2022-04-25 02:52:33 -0400

    settingsentry javadocs

--- src/main/java/oogasalad/engine/view/Popup/SettingsView/SettingsEntry/MouseSoundEntry.java
+++ src/main/java/oogasalad/engine/view/Popup/SettingsView/SettingsEntry/MouseSoundEntry.java
@@ -4,10 +4,21 @@ import oogasalad.engine.view.OptionSelect.OptionSelect;
 
 
 /**
+ *
+ * settings entry specific for mouse sound selection
+ * inherits from SettingsEntry
+ *
  * @author Cynthia France
  */
 public class MouseSoundEntry extends SettingsEntry {
 
+  /**
+   *
+   * creates a settings entry for mouse sound selection
+   *
+   * @param txt what the label should display
+   * @param select the dropdown menu
+   */
   public MouseSoundEntry(String txt, OptionSelect select) {
     super(txt, select);
   }
--- src/main/java/oogasalad/engine/view/Popup/SettingsView/SettingsEntry/ThemeSoundEntry.java
+++ src/main/java/oogasalad/engine/view/Popup/SettingsView/SettingsEntry/ThemeSoundEntry.java
@@ -3,10 +3,21 @@ package oogasalad.engine.view.Popup.SettingsView.SettingsEntry;
 import oogasalad.engine.view.OptionSelect.OptionSelect;
 
 /**
+ *
+ * settings entry specific for theme selection
+ * inherits from SettingsEntry
+ *
  * @author Cynthia France
  */
 public class ThemeSoundEntry extends SettingsEntry {
 
+  /**
+   *
+   * creates a settings entry for theme selection
+   *
+   * @param txt what the label should display
+   * @param select the dropdown menu
+   */
   public ThemeSoundEntry(String txt, OptionSelect select) {
     super(txt, select);
   }
