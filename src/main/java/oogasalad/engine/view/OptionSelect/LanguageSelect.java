c30899f03a6f1ad98d3618fc580dcdfb8bb6b7b0
Cynthia France <cf219@duke.edu>
2022-04-25 02:37:04 -0400

    optionSelect javadocs

--- src/main/java/oogasalad/engine/view/OptionSelect/LanguageSelect.java
+++ src/main/java/oogasalad/engine/view/OptionSelect/LanguageSelect.java
@@ -2,15 +2,25 @@ package oogasalad.engine.view.OptionSelect;
 
 
 /**
+ *
+ * Dropdown menu for CSS/theme selection
+ * Inherits from OptionSelect
+ *
  * @author Cynthia France
  */
 public class LanguageSelect extends OptionSelect {
 
+  /**
+   * creates a selection menu for different language selection
+   */
   public LanguageSelect() {
     super();
     this.setId("language-select");
   }
 
+  /**
+   * updates options with the correct values
+   */
   @Override
   protected void setup() {
     options = new String[]{"English", "Spanish", "French", "German"};
--- src/main/java/oogasalad/engine/view/OptionSelect/MouseSoundSelect.java
+++ src/main/java/oogasalad/engine/view/OptionSelect/MouseSoundSelect.java
@@ -3,14 +3,24 @@ package oogasalad.engine.view.OptionSelect;
 import oogasalad.engine.view.MouseSound;
 
 /**
+ *
+ * Dropdown menu for CSS/theme selection
+ * Inherits from OptionSelect
+ *
  * @author Cynthia France
  */
 public class MouseSoundSelect extends OptionSelect {
 
+  /**
+   * creates a selection menu for different mouse sound selection
+   */
   public MouseSoundSelect() {
     super();
   }
 
+  /**
+   * updates options with the correct values, read in from MouseSound.SOUNDS
+   */
   @Override
   protected void setup() {
     options = new String[MouseSound.SOUNDS.length + 1];
