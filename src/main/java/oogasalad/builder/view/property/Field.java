0e43e9c85a39f88268e5e4f74238d70da074911e
Mike Keohane <mbk28@duke.edu>
2022-04-24 23:26:40 -0400

    added mapping to value for textfields

--- src/main/java/oogasalad/builder/view/property/Field.java
+++ src/main/java/oogasalad/builder/view/property/Field.java
@@ -3,8 +3,10 @@ package oogasalad.builder.view.property;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
 import oogasalad.builder.model.property.Property;
-import oogasalad.builder.model.property.StringProperty;
+import oogasalad.builder.view.ViewResourcesSingleton;
 import oogasalad.builder.view.callback.CallbackDispatcher;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
 
 /**
  * The most basic form of a PropertySelector, essentially just a wrapper for a JavaFX text box.
@@ -23,10 +25,22 @@ public abstract class Field implements PropertySelector{
    */
   public Field(Property property, CallbackDispatcher dispatcher){
     this.property = property;
-    valueField = new TextField(property.valueAsString());
+    valueField = new TextField(tryGetResourceString(property.valueAsString()));
     valueField.setId("stringField-" + property.shortName());
   }
 
+  /**
+   * Trys to get key from ViewResourceSingleton and logs if it cant
+   * @return String mapped value or key if not
+   */
+  protected String tryGetResourceString(String key){
+    try{ ViewResourcesSingleton.getInstance().getString(key);
+
+    }catch(Exception e){
+      LogManager.getLogger().log(Level.ERROR, e.getMessage());
+    }
+    return key;
+  }
   /**
    * Returns a JavaFX Node that will be displayed to the user next to the property label
    *
