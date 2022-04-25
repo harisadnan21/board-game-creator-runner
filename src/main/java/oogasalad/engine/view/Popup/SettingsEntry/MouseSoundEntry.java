8c070d6aa24c6afbf3de7064d32d37cce88cdc17
Cynthia France <cf219@duke.edu>
2022-04-25 00:33:19 -0400

    create theme box for settings menu

--- src/main/java/oogasalad/engine/view/Popup/SettingsEntry/MouseSoundEntry.java
+++ src/main/java/oogasalad/engine/view/Popup/SettingsEntry/MouseSoundEntry.java
@@ -1,6 +1,7 @@
-package oogasalad.engine.view.Popup;
+package oogasalad.engine.view.Popup.SettingsEntry;
 
 import oogasalad.engine.view.OptionSelect.OptionSelect;
+import oogasalad.engine.view.Popup.SettingsEntry.SettingsEntry;
 
 public class MouseSoundEntry extends SettingsEntry {
 
---
+++ src/main/java/oogasalad/engine/view/Popup/SettingsEntry/ThemeSoundEntry.java
@@ -0,0 +1,11 @@
+package oogasalad.engine.view.Popup.SettingsEntry;
+
+import oogasalad.engine.view.OptionSelect.OptionSelect;
+import oogasalad.engine.view.Popup.SettingsEntry.SettingsEntry;
+
+public class ThemeSoundEntry extends SettingsEntry {
+
+  public ThemeSoundEntry(String txt, OptionSelect select) {
+    super(txt, select);
+  }
+}
