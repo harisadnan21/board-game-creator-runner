bf6e60cd65e7c4c26b002a31c6415ca098b93945
Robert Cranston <rvc6@duke.edu>
2022-04-25 02:17:48 -0400

    added comments to cheat codes

--- src/main/java/oogasalad/engine/cheat_codes/PlayerTwoWins.java
+++ src/main/java/oogasalad/engine/cheat_codes/PlayerTwoWins.java
@@ -3,8 +3,18 @@ package oogasalad.engine.cheat_codes;
 import oogasalad.engine.controller.Controller;
 import oogasalad.engine.model.board.Board;
 
+/**
+ * Sets player two to the winner and ends the game
+ * @author Robert Cranston
+ */
 public class PlayerTwoWins implements CheatCode{
 
+  /**
+   * executes the cheat code based on the given board. Updates the board through the controller.
+   * @param board current game board
+   * @param controller controller for the
+   * @return  board with the update after the cheat code
+   */
   @Override
   public Board accept(Board board, Controller controller) {
     controller.getEngine().endGame(1);
--- src/main/java/oogasalad/engine/cheat_codes/RemovePlayer1Piece.java
+++ src/main/java/oogasalad/engine/cheat_codes/RemovePlayer1Piece.java
@@ -6,9 +6,18 @@ import oogasalad.engine.model.board.Board;
 import oogasalad.engine.model.board.cells.Position;
 import oogasalad.engine.model.board.cells.PositionState;
 
-
+/**
+ * Removes a piece from player one's pieces
+ * @author Robert Cranston
+ */
 public class RemovePlayer1Piece implements CheatCode{
 
+  /**
+   * executes the cheat code based on the given board. Updates the board through the controller.
+   * @param board current game board
+   * @param controller controller for the
+   * @return  board with the update after the cheat code
+   */
   @Override
   public Board accept(Board board, Controller controller) {
     Optional<Position> pos = (board.getPositionStatesStream()
