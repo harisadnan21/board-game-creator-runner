34dd086972419b05ebc2c6e522f43eb098a3d4a3
Mike Keohane <mbk28@duke.edu>
2022-04-29 13:36:45 -0400

    made test to test uneven board shapes

--- src/test/java/oogasalad/builder/BuilderTest.java
+++ src/test/java/oogasalad/builder/BuilderTest.java
@@ -1,5 +1,45 @@
 package oogasalad.builder;
 
-public class BuilderTest {
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import oogasalad.builder.controller.BuilderController;
+import oogasalad.builder.view.BuilderView;
+import oogasalad.builder.view.callback.CallbackDispatcher;
+import oogasalad.builder.view.callback.GetHeightCallback;
+import oogasalad.builder.view.callback.GetWidthCallback;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.Test;
+import util.DukeApplicationTest;
+
+public class BuilderTest extends DukeApplicationTest {
+  private static final int X_DIM = 10;
+  private static final int Y_DIM = 14;
+
+  private CallbackDispatcher callbackDispatcher;
+  @Override
+  public void start(Stage stage) {
+    new BuilderController(new BuilderView(stage));
+    callbackDispatcher = new CallbackDispatcher();
+  }
+
+
+  @Test
+  public void testUnevenBoardShapes(){
+    clickOn("#boardTab");
+    var xSpinner = lookup("#xDimEntry").queryAs(Spinner.class);
+    var ySpinner = lookup("#yDimEntry").queryAs(Spinner.class);
+    xSpinner.getEditor().setText("" + X_DIM);
+    ySpinner.getEditor().setText("" + Y_DIM);
+    xSpinner.commitValue();
+    ySpinner.commitValue();
+    lookup("#colorPickerA").queryAs(ColorPicker.class).setValue(Color.BLUE);
+    assertEquals(callbackDispatcher.call(new GetHeightCallback()).orElseThrow(), Y_DIM);
+    assertEquals(callbackDispatcher.call(new GetWidthCallback()).orElseThrow(), X_DIM);
+  }
 }
