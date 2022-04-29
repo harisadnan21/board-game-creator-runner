bc88299eaa5b7c4748f195bbad508a1f08a386f0
Cynthia France <cf219@duke.edu>
2022-04-29 17:46:00 -0400

    Summary of the feature's bug (without describing implementation details)
    Clicking on the engine button should open the engine in a new window.
    
    Opening the engine does not make a new stage.
    
    Provide detailed steps for reproducing the issue.
    
     1. run Main
     2. click on "engine" button
    
    None
    
    create the engine application in a new stage.

--- src/test/java/oogasalad/view/SplashWelcomeTest.java
+++ src/test/java/oogasalad/view/SplashWelcomeTest.java
@@ -1,12 +1,36 @@
 package oogasalad.view;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.DukeApplicationTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SplashWelcomeTest {
+class SplashWelcomeTest extends DukeApplicationTest {
 
-    @BeforeEach
-    void setUp() {
+    SplashWelcome newWelcome;
+    Stage s;
+
+    @Override
+    public void start (Stage stage) throws IOException {
+        s = stage;
+        newWelcome = new SplashWelcome();
+    }
+
+    @Test
+    void test() throws NoSuchFieldException, IllegalAccessException {
+        Field buttonField = SplashWelcome.class.getDeclaredField("engine");
+        buttonField.setAccessible(true);
+        Field stageField = SplashWelcome.class.getDeclaredField("stage");
+        stageField.setAccessible(true);
+        Button b = (Button)buttonField.get(newWelcome);
+        Stage stage = (Stage)stageField.get(newWelcome);
+        clickOn(b);
+        assertEquals(678, stage.getHeight());
     }
 }
