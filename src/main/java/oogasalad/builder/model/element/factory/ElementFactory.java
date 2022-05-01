9ded7ad1de24fe944feddfc9b8a8a259e8a097dc
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-05-01 15:17:08 -0400

    Added commits and design rationale to ElementFactory interface

--- src/main/java/oogasalad/builder/model/element/factory/ElementFactory.java
+++ src/main/java/oogasalad/builder/model/element/factory/ElementFactory.java
@@ -12,6 +12,16 @@ import oogasalad.builder.model.property.Property;
  * API For creating a game element based on its properties. Provides methods for creating elements
  * and listing required properties.
  *
+ * This is class is well-designed as it illustrates the Interface Segregation Principle and provides
+ * an API for creating Game Elements. It also allows us to create an Abstract Factory Pattern for
+ * creating Game Elements.
+ *
+ * Commits:
+ * https://coursework.cs.duke.edu/compsci308_2022spring/oogasalad_OOGABOOGA/-/commit/55505ff7e5c7f6eb167956c73a3972cc0dc5bc50
+ * https://coursework.cs.duke.edu/compsci308_2022spring/oogasalad_OOGABOOGA/-/commit/aa761c5f7e0566063d335ff40994d794ffba2b94
+ * https://coursework.cs.duke.edu/compsci308_2022spring/oogasalad_OOGABOOGA/-/commit/77c01f880cc39ff3457d6107f0d138be14f56db5
+ * https://coursework.cs.duke.edu/compsci308_2022spring/oogasalad_OOGABOOGA/-/commit/55860a9e82e93c901098a10b9ad5c1fd8b083258
+ *
  * @author Shaan Gondalia
  */
 public interface ElementFactory extends JSONParseable<Element> {
