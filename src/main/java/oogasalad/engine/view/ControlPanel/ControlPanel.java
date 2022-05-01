c705a8904532508d7dce48455628420f3c9cb3a5
Cynthia France <cf219@duke.edu>
2022-05-01 01:33:04 -0400

    refactored ControlPanel

--- src/main/java/oogasalad/engine/view/ControlPanel/ControlPanel.java
+++ src/main/java/oogasalad/engine/view/ControlPanel/ControlPanel.java
@@ -2,9 +2,6 @@ package oogasalad.engine.view.ControlPanel;
 
 import java.util.ResourceBundle;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 /**
@@ -15,9 +12,7 @@ import javafx.scene.layout.VBox;
 public abstract class ControlPanel {
   public static final String DEFAULT_RESOURCE_PACKAGE = "/engine-view/resource-names/";
   public static final String DEFAULT_LANGUAGE_RESOURCE_PACKAGE = "/engine-view/languages/";
-  public static ResourceBundle imBundle = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "Image");
   public static String IMAGES_FOLDER = "/engine-view/images/";
-  public static int ICON_SIZE = 25;
 
   protected VBox root;
   protected ResourceBundle myResources;
@@ -31,7 +26,6 @@ public abstract class ControlPanel {
     myResources = ResourceBundle.getBundle(DEFAULT_LANGUAGE_RESOURCE_PACKAGE + language);
     root = new VBox();
     root.setId("cp-root");
-    createButtons();
   }
 
   /**
@@ -45,16 +39,5 @@ public abstract class ControlPanel {
   }
 
   //creates all the buttons in a given panel
-  protected abstract void createButtons();
-
-  //creates a singular button in the panel with an image from the given image path
-  protected Button createButton(String imagePath) {
-    ImageView buttonImage = new ImageView(new Image(imagePath));
-    buttonImage.setId("cp-button-image");
-    buttonImage.setFitWidth(ICON_SIZE);
-    buttonImage.setFitHeight(ICON_SIZE);
-    Button b = new Button("", buttonImage);
-    b.setId("cp-button");
-    return b;
-  }
+  protected abstract void createButtons(String language);
 }
